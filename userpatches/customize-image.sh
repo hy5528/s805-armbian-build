1ba69edda86c086298b201f67ef624be175790df
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-12-16 09:59:05 +0800

    Add files via upload

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -23,7 +23,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 
 # 清理缓存

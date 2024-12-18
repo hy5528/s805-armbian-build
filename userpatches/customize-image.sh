53115bdedf3a66eed85d31b1a5233b1efc534ca5
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-12-18 08:43:16 +0800

    Add files via upload

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -13,11 +13,20 @@ deb http://ports.ubuntu.com/ jammy-updates main restricted universe multiverse
 deb http://ports.ubuntu.com/ jammy-backports main restricted universe multiverse
 #deb-src http://ports.ubuntu.com/ jammy-backports main restricted universe mult>
 EOF
-# 更新软件
+# 更新软件包
 sudo apt-get update
 
+# 安装 Docker
 sudo apt-get install -y ca-certificates curl gnupg lsb-release alsa-utils 
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.1/docker-compose-linux-armv7" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # 清理缓存
 apt autoremove -y && apt autoclean && apt remove -y && apt clean
